0cc0d5b27ef690517b6c0d623cf1c447b403e9b9
bop <105178263+bopthing1@users.noreply.github.com>
2022-07-22 18:23:43 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,11 @@
+local instance = require(game.ServerScriptService.instance)
+
+local instance = instance:new("Part", workspace)
+instance.Name = "Test"
+local instance2 = instance:new("Decal", instance)
+instance2.Name = "TestDecal"
+instance2.Texture = "rbxassetid://3847925"
+
+print(instance:recallAllInstancesMadeByModule()) --expected output: {Test}
+instance:destroyAllInstancesMadeByModule("Part") --only should delete instance1
 
